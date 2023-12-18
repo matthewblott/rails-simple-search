79a8ec7e259a09c137d8a107b2a4e4640123a2b6
Matthew Blott <m4ttb@pm.me>
2023-12-18 21:05:20 +0000

    Made the fields clear on selection

--- app/javascript/controllers/results_controller.js
+++ app/javascript/controllers/results_controller.js
@@ -1,9 +1,12 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
+  static targets = ['the_list']
+
   select(event) {
     const field = this.data.get('field-param')
     const value = event.currentTarget.innerText
     this.dispatch('selected', { detail: { field, value } })
+    this.the_listTarget.innerHTML = ''
   }
 }
